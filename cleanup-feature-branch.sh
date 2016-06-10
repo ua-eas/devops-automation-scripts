cf7d3b564a68c16779b760ac137adaf2a0358720
William Austin <quikkian@email.arizona.edu>
2016-06-10 10:36:55 -0700

    Adding script to cleanup a list of branches provided by the user, as well as a test helper script to create branches (to later close) from a list of branches provided by the user.

---
+++ cleanup-feature-branch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+kfsRepoUrl='git@github.com:ua-eas/kfs-devops-automation-fork.git'
+# Prompt user for different URLs, if needed
+echo -n "Input KFS repo URL (default: $kfsRepoUrl): "
+read inputKfsRepoUrl
+if [[ $inputKfsRepoUrl ]]; then
+    echo "Using KFS repo Url: $inputKfsRepoUrl"
+    kfsRepoUrl=$inputKfsRepoUrl
+fi
+echo -n "Enter a comma-separated list of branches to clean up (i.e. 'UAF-AAA,UAF-BBB,...,UAF-ZZZ') : "
+IFS=',' read -ra branches
+# Remove any existing temporary directory
+rm -Rf /tmp/repo
+
+git clone $kfsRepoUrl /tmp/repo/kfs
+cd /tmp/repo/kfs
+
+# For each branch, tag it as archive/<branchname>,
+#   push the archived tag, and delete the branch.
+for branch in "${branches[@]}"; do
+    echo "Processing branch $branch"
+    git checkout -b $branch origin/$branch
+    git tag archive/$branch $branch
+    git push origin --tags
+    git push origin --delete $branch
+    git checkout ua-development
+    git branch -d $branch
+done
